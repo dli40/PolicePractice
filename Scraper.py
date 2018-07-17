88a2ff85ed88866c44e94b3140064138e9c93418
Daniel Li <dli40@calpoly.edu>
2018-07-16 20:47:31 -0700

    Update Scraper.py

--- Scraper.py
+++ Scraper.py
@@ -1,17 +1,12 @@
 from bs4 import BeautifulSoup
-public class Scraper
-{
-   def main():
+from urllib.request import urlopen
 
-
-   if name== "__main__":
-      main()   
-
-
-
-
+class Scraper:
+    url = 'https://www.census.gov/quickfacts/fact/table/alamedacountycalifornia/PST045217'
+    response = urlopen(url)
+    string = response.read().decode('utf-8') #STRING CONTAINS LITERALLY EVERYTHING IN THE URL
+    soup = BeautifulSoup(string, 'html5lib') #converting to beautifulsoujp object to use methods
+    print(soup.title)
 
 
 
-}
- 
