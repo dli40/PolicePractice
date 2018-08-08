599e15646c9c500511bfa93fdbb312b314361467
Daniel Li <dli40@calpoly.edu>
2018-08-08 11:32:10 -0700

    Update weather.sql

--- weather.sql
+++ weather.sql
@@ -1,5 +1,5 @@
-CREATE EXTERNAL TABLE `slweather_json`(
-  `array` array<struct<date:string,maxtempc:string,maxtempf:string,mintempc:string,mintempf:string,totalsnow_cm:string,sunhour:string,uvindex:string,sunrise:string,sunset:string,moonrise:string,moonset:string,moon_phase:string,moon_illumination:string,time:string,tempc:string,tempf:string,windspeedmiles:string,windspeedkmph:string,winddirdegree:string,winddir16point:string,weathercode:string,weathericonurl:array<struct<value:string>>,weatherdesc:array<struct<value:string>>,precipmm:string,humidity:string,visibility:string,pressure:string,cloudcover:string,heatindexc:string,heatindexf:string,dewpointc:string,dewpointf:string,windchillc:string,windchillf:string,windgustmiles:string,windgustkmph:string,feelslikec:string,feelslikef:string>> COMMENT 'from deserializer')
+CREATE EXTERNAL TABLE `weathersmall_json`(
+  `array` array<struct<date:string,maxtempc:string,maxtempf:string,mintempc:string,mintempf:string,totalsnow_cm:string,sunhour:string,uvindex:string,sunrise:string,sunset:string,moonrise:string,moonset:string,moon_phase:string,moon_illumination:string,time:string,tempc:string,tempf:string,windspeedmiles:string,windspeedkmph:string,winddirdegree:string,winddir16point:string,weathercode:string,precipmm:string,humidity:string,visibility:string,pressure:string,cloudcover:string,heatindexc:string,heatindexf:string,dewpointc:string,dewpointf:string,windchillc:string,windchillf:string,windgustmiles:string,windgustkmph:string,feelslikec:string,feelslikef:string>> COMMENT 'from deserializer')
 ROW FORMAT SERDE 
   'org.openx.data.jsonserde.JsonSerDe' 
 WITH SERDEPROPERTIES ( 
@@ -9,14 +9,15 @@ STORED AS INPUTFORMAT
 OUTPUTFORMAT 
   'org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat'
 LOCATION
-  's3://police-store-again/JSON/SLweather.json'
+  's3://police-store-again/JSON/weatherSmall.json'
 TBLPROPERTIES (
   'CrawlerSchemaDeserializerVersion'='1.0', 
   'CrawlerSchemaSerializerVersion'='1.0', 
-  'UPDATED_BY_CRAWLER'='yelpBoi', 
-  'averageRecordSize'='675437', 
+  'UPDATED_BY_CRAWLER'='weatherBoi', 
+  'averageRecordSize'='555292', 
   'classification'='json', 
   'compressionType'='none', 
+  'objectCount'='1', 
   'recordCount'='1', 
-  'sizeKey'='675437', 
+  'sizeKey'='555292', 
   'typeOfData'='file')
